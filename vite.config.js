5a8018305371495259dd2c68d1c12feb1cc527a9
SUJI-J <suzy852@naver.com>
2024-09-06 18:25:10 +0900

    chore: 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { staticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    staticCopy({
+      targets: [
+        {
+          src: 'assets/**/*',
+          dest: 'assets'    
+        }
+      ]
+    })
+  ],
+  publicDir: 'public',
 })
+
